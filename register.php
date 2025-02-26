bb20204c3833ecf9fe0e62cbdebae94abc7b1b31
angellee26 <lcacapol23-0372@cca.edu.ph>
2025-02-26 18:39:44 +0800

    FORM SUBMISSION
    terminal page

--- register.php
+++ register.php
@@ -3,15 +3,16 @@
         <title>Form Submission</title>
     </head>
     <body>
-        <form action = "action.php" method="GET">
-            <table>
+        <h1> ADD STUDENT </h1>
+        <form action = "action_page.php" method="POST">
+            <table border="1" cellspacing="5" width="30%">
                 <tr>
                     <th>Question</th>
                     <th>Answer</th>
                 </tr>
                 <tr>
                     <td>Name</td>
-                    <td> <input type = "text" Name = Name></td>
+                    <td> <input type = "text" Name = "FullName" placeholder="Enter Your FullName"></td>
                 </tr>
                 <tr>
                     <td>Gender</td>
@@ -26,6 +27,7 @@
                     <td>Country</td>
                     <td>
                         <select name = "Country">
+                        <option value = "Select Country"> -Select Country- </option>
                         <option value = "PH"> Philippines </option>
                         <option value = "AUS"> Australia </option>
                         <option value = "US"> United States </option>
@@ -33,7 +35,9 @@
                     </td>
                 </tr>
                 <tr>
-                    <td colspan = "2"> <input type = "Submit" value = "Save"> </td>
+                    <td colspan = "2" align="right">
+                        <input style = "padding:1@px; color: #FFF; background-color: blue"
+                        type = "Submit" name = "save" value = "Save"/> </td>
                 </tr>
             </table>
         </form>
